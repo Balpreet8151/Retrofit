8f43febf73e53808a1de77857a48ac93e1d32c9b
balpreetsingh <balpreet.singh@agicent.in>
2022-08-09 15:21:09 +0530

    u

--- app/src/main/java/com/example/retrofittutorial/ApiInterface.kt
+++ app/src/main/java/com/example/retrofittutorial/ApiInterface.kt
@@ -1,11 +1,19 @@
 package com.example.retrofittutorial
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface ApiInterface {
 
     @GET("posts")
     fun getData() : Call<List<MyDataItem>>
+
+    @POST("user/classes")
+    fun addToPlaylist(
+        @Header("Content-Type") content_type: String?): Response<MyDataItem?>?
+
 
 }
--- app/src/main/java/com/example/retrofittutorial/MainActivity.kt
+++ app/src/main/java/com/example/retrofittutorial/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL= "https://jsonplaceholder.typicode.com/"
+const val BASE_URL= "https://api-basketball.p.rapidapi.com/"
 class MainActivity : AppCompatActivity() {
 
 
@@ -28,6 +28,18 @@ class MainActivity : AppCompatActivity() {
 
         val retrofitData = retrofitBuilder.getData()
 
+
+
+
+
+
+
+
+
+
+
+
+
         retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
             override fun onResponse(
                 call: Call<List<MyDataItem>?>,
@@ -35,6 +47,7 @@ class MainActivity : AppCompatActivity() {
             ) {
 
                 val responseBody = response.body()
+
                 val textView = findViewById<TextView>(R.id.textView)
                 responseBody?.let {
                     textView.text= it[0].id.toString()
@@ -51,4 +64,56 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+
+
+     private fun getData(){
+
+
+     lifecycleScope.launch{
+         //show loader
+         val result = apiInterface.getListingCategories()
+
+         if(result.isSuccessful){
+             //success
+             val response = result.body()
+             response?.let {
+                 //
+                 //list.addAll(it.data)
+                 //adapter.notify
+               val propertyValuesJson = it.data?.get(0)?.categoryFieldData?.get(1)?.fieldValues
+
+                 val propertiesList = propertyValuesJson?.toArrayList<PropertyTypeModelClass>()
+                 if (propertiesList != null) {
+                     for (propertyData in propertiesList){
+                         propertyType.add(propertyData.name)
+                     }
+                 }
+
+                 val termsValueJson =  it.data?.get(0)?.categoryFieldData?.get(10)?.fieldValues
+                 val termsList = termsValueJson?.toArrayList<PropertyTypeModelClass>()
+                 if (termsList != null) {
+                     for (termsData in termsList){
+                         terms.add(termsData.name)
+                     }
+                 }
+
+
+             setUpSpinner()
+
+                 showToast(requireContext(),propertyType.size.toString())
+
+             }
+
+         }else{
+             //error
+             //show error msg
+             showToast(requireContext(),"Error")
+         }
+     }
+
+ }
+ }
+
+
 }
